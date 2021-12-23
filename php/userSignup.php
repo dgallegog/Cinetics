e66c5c3d0fa452b08ee329a73de2bbb6085df32e
yudokusora <45208610+yudokusora@users.noreply.github.com>
2021-12-23 19:53:24 +0100

    Update userSignup.php

---
+++ php/userSignup.php
@@ -0,0 +1,29 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    if(isset($_POST['username']) && isset($_POST['password']) && isset($_POST['firstname']) && isset($_POST['lastname']) && isset($_POST['email']))
+    {
+        registerOk(filter_input(INPUT_POST,'username'),filter_input(INPUT_POST,'password'),filter_input(INPUT_POST,'firstname'),filter_input(INPUT_POST,'lastname'),filter_input(INPUT_POST,'email'));
+    }
+}
+
+
+function registerOk ($user,$pwd,$firstname,$lastname,$email)
+{
+    require_once('conexionDB.php'); 
+   
+    $pwd = password_hash($pwd,PASSWORD_DEFAULT);
+    $sql = "INSERT INTO `users`(mail, username, passHash, userFirstName, userLastName) VALUES ('$email','$user','$pwd','$firstname','$lastname')";
+    $insert = $db->query($sql);
+    
+    if($insert)
+    {
+        echo '<p> insert correcto</p>';
+    }else{
+        print_r($db->errorinfo());
+    }
+    
+   
+    
+}
