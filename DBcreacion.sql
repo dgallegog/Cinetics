492fea47718e641a3892947a60d34fa74104ae0f
yudokusora <45208610+yudokusora@users.noreply.github.com>
2022-01-14 09:14:32 +0100

    Add files via upload

--- DBcreacion.sql
+++ DBcreacion.sql
@@ -12,13 +12,12 @@ CREATE TABLE IF NOT EXISTS `users`(
     `creationDate` DATETIME DEFAULT NOW(), 
     `removeDate` DATETIME,
     `lastSignIn` DATETIME,
-    `active` TINYINT(1),
+    `active` TINYINT(1) DEFAULT 0,
     `is_admin` TINYINT(1) NOT NULL DEFAULT 0,
-    `activation_code` VARCHAR(255) NOT NULL,
-    `activation_expiry` DATETIME NOT NULL,
-    `activated_at` DATETIME DEFAULT NULL,
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-    `updated_at` DATETIME DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    `activationCode` VARCHAR(255) NOT NULL,
+    `activationExpiry` DATETIME NOT NULL,
+    `activationDate` DATETIME DEFAULT NULL,
+    `resetPassCode` VARCHAR(255) NOT NULL,
     PRIMARY KEY(`iduser`)
 );
 
